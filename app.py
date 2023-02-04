311cf7a3651265e3b49e6ce0e39eb732332fc284
Muneeb Asmone <92797864+milenek-x@users.noreply.github.com>
2023-02-04 17:04:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def ecs1():
 	return render_template("ecs1-videos.html")
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=3000, debug=True)
